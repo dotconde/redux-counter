642fddc26be5d2df11e97d8dc18c8dee3f645951
Deyvi Conde <iamdeyvi@gmail.com>
2021-01-10 12:58:09 -0500

    Add actions deposit and withdraw to Counter form

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,12 +1,13 @@
 import React, { useRef } from "react";
+import { deposit, withdraw } from "../../store/amount/action";
 
 function Counter() {
   const amountRef = useRef(null);
   return (
     <div>
       <h1 ref={amountRef}> Value... </h1>
-      <button>Deposit $10</button>
-      <button>Withdraw $10</button>
+      <button onClick={() => deposit()}>Deposit $10</button>
+      <button onClick={() => withdraw()}>Withdraw $10</button>
     </div>
   );
 }
