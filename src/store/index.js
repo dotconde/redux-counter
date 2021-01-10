7f359978f627317ccdc96600e616d6ed467c32b4
Deyvi Conde <iamdeyvi@gmail.com>
2021-01-10 14:00:31 -0500

    Create Store and setup with Redux Tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,14 @@
 import { createStore, combineReducers } from "redux";
+
+import amountReducer from "./amount/reducer";
+
+const reducers = combineReducers({
+  amountReducer,
+});
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
