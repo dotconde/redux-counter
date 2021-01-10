af2ece827edcc79c4c25de3155e24bb425cc2187
Deyvi Conde <iamdeyvi@gmail.com>
2021-01-10 13:34:04 -0500

    Add reducer for amount (withdraw and deposit)

---
+++ src/store/amount/reducer.js
@@ -0,0 +1,18 @@
+const initialState = { amount: 0 };
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+  if (action.type === "DEPOSIT_MONEY") {
+    return {
+      ...state,
+      amount: state.amount + 10,
+    };
+  }
+  if (action.type === "WITHDRAW_MONEY") {
+    return {
+      ...state,
+      amount: state.amount - 10,
+    };
+  }
+  return state;
+};
