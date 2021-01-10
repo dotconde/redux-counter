b520314e06834cf9b2fcbda8aad875d698beaa7a
Deyvi Conde <iamdeyvi@gmail.com>
2021-01-09 21:53:32 -0500

    Add Counter Component with Airbnb Stylish

---
+++ src/Counter/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Counter() {
+  return (
+    <div>
+      <h1> Value... </h1>
+      <button> Add </button>
+      <button> Decrease </button>
+    </div>
+  );
+}
+
+export default Counter;
