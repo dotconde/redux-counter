1c2996b143a1d3afe735efab10fbcd983acf55ed
Deyvi Conde <iamdeyvi@gmail.com>
2021-01-10 18:14:49 -0500

    Add mapStateToProps and get state from Redux Store

--- src/components/counter/index.jsx
+++ src/components/counter/index.jsx
@@ -1,16 +1,21 @@
-import React, { useRef } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { deposit, withdraw } from "../../store/amount/action";
 
-function Counter({ deposit, withdraw }) {
-  const amountRef = useRef(null);
+const mapStateToProps = (state) => {
+  return {
+    amount: state.amountReducer.amount,
+  };
+};
+
+function Counter({ amount, deposit, withdraw }) {
   return (
     <div>
-      <h1 ref={amountRef}> Value... </h1>
+      <h1> {amount} </h1>
       <button onClick={() => deposit()}>Deposit $10</button>
       <button onClick={() => withdraw()}>Withdraw $10</button>
     </div>
   );
 }
 
-export default connect(null, { deposit, withdraw })(Counter);
+export default connect(mapStateToProps, { deposit, withdraw })(Counter);
